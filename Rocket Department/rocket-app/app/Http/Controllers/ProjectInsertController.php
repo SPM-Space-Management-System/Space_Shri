ae6052e301aa27e1d2f9bae907725cb772473745
RCP Rajapaksha <it20137250@my.sliit.lk>
2022-08-18 16:57:11 +0530

    Add facade design patterns

--- Rocket Department/rocket-app/app/Http/Controllers/ProjectInsertController.php
+++ Rocket Department/rocket-app/app/Http/Controllers/ProjectInsertController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
+use domain\Facades\ProjectInsertFacade;
 use Illuminate\Http\Request;
 
 class ProjectInsertController extends Controller
@@ -9,4 +11,10 @@ class ProjectInsertController extends Controller
     public function projectinsertview() {
         return view('pages.Project.projectinsert');
     }
+
+    public function store(Request $request) {
+        ProjectInsertFacade::store($request->all());
+        return redirect()->back()->with('message','Details Added Successfully!');
+    }
+
 }

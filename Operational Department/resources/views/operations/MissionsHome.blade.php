0b69ed529cebe9567f1485a07ba51b29f06b0742
IT20230692 <IT20230692@my.sliit.lk>
2022-09-02 22:41:12 +0530

    fix mission home

--- Operational Department/resources/views/operations/MissionsHome.blade.php
+++ Operational Department/resources/views/operations/MissionsHome.blade.php
@@ -29,23 +29,25 @@
   <a href="#" class="btn btn btn-success" type="submit">Generate Report</a>
   
   <div class="space" style="padding-top: 5vh"></div>
-  
-    <div class="row mb-2" class="rounded">
+  <h3>Latest Posts</h3>
+    <div class="row mb-2 " class="rounded">
       @foreach ($missionsALL as $mission)  
-      <div class="col-md-6">
+      <div class="col-md-6 p-3 mb-2 bg-dark text-white">
         <div class="card" class="border border-primary"></div>
 
-        <div class="rounded"></div>
+        <div class="rounded bgcolo"></div>
         <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" class="border border-primary">
           <img class="card-img-top" src="{{ asset('thumbnails/' . $mission->mission_image) }}" width="200" height="200"  alt="missions images">
-            <div class="col p-4 d-flex flex-column position-static">
+            <div class="col p-3 d-flex flex-column position-static">
               <div class="aligns">
-                <div class="mb-1 text-muted">{{date('Y-m-d',strtotime($mission->created_at))}}</div> 
+                <div class="mb-1 text-muted text-dark text-primary">{{date('Y-m-d',strtotime($mission->created_at))}}</div> 
               </div>
                 
                 <h2 class="mb-0">{{ $mission->topic}}</h2>
                 <div class="mb-1 text-muted">Last Update Date : {{date('Y-m-d',strtotime($mission->updated_at))}} </div>
-                <a href="{{ route('operationsShow', $mission->mission_id) }}" class="stretched-link">Continue reading</a>
+                <a href="{{ route('operationsShow', $mission->mission_id) }}" >Continue reading</a>
+                {{-- <a href="{{ route('operationsShow', $mission->mission_id) }}"  class="btn btn bg-primary" type="submit">Update/Remove Post</a> --}}
+                <a href="#"class="btn btn bg-warning" type="submit">Update/Remove Post</a>
             </div>
            <div class="col-auto d-none d-lg-block"></div>
         </div>
@@ -80,9 +82,12 @@ body{
         background-repeat: no-repeat;
         background-attachment: fixed;
         background-size: 100% 100%;
-        background-color: rgb(15, 15, 15)
+        background-color: rgb(15, 15, 15) 
 
         /* background-color: rgb(12, 3, 7) */
+    }
+    h3{
+      color: rgb(247, 16, 93);
     }
 </style> 
 @endpush

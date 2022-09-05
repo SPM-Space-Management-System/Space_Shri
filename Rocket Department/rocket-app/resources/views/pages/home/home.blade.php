24777c8f7049de0dda4a010a0df69940123c8890
RCP Rajapaksha <it20137250@my.sliit.lk>
2022-09-05 18:16:17 +0530

    Creating rocket list page

--- Rocket Department/rocket-app/resources/views/pages/home/home.blade.php
+++ Rocket Department/rocket-app/resources/views/pages/home/home.blade.php
@@ -133,7 +133,30 @@
         <div class="swiper-pagination"></div>
     </div>
     <div class="cont">
-        <div class="cards"></div>
+        <div class="cards">
+            <div class="personBx">
+                <img src="waru.jpg">
+            </div>
+            <div class="content">
+                <div class="details">
+                    <h2>Waruni Bhagya<br><span>Article Department</span></h2>
+                    <ul class="social_icons">
+                        <li><a href="#">
+                                <ion-icon name="logo-facebook"></ion-icon>
+                            </a></li>
+                        <li><a href="#">
+                                <ion-icon name="logo-instagram"></ion-icon>
+                            </a></li>
+                        <li><a href="#">
+                                <ion-icon name="logo-twitter"></ion-icon>
+                            </a></li>
+                        <li><a href="#">
+                                <ion-icon name="logo-linkedin"></ion-icon>
+                            </a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
         <div class="cards">
             <div class="personBx">
                 <img src="img1.jpg">
@@ -182,7 +205,30 @@
                 </div>
             </div>
         </div>
-        <div class="cards"></div>
+        <div class="cards">
+            <div class="personBx">
+                <img src="dasu.jpg">
+            </div>
+            <div class="content">
+                <div class="details">
+                    <h2>Dasunika Sewwandi<br><span>Jobs & Career Department</span></h2>
+                    <ul class="social_icons">
+                        <li><a href="#">
+                                <ion-icon name="logo-facebook"></ion-icon>
+                            </a></li>
+                        <li><a href="#">
+                                <ion-icon name="logo-instagram"></ion-icon>
+                            </a></li>
+                        <li><a href="#">
+                                <ion-icon name="logo-twitter"></ion-icon>
+                            </a></li>
+                        <li><a href="#">
+                                <ion-icon name="logo-linkedin"></ion-icon>
+                            </a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 

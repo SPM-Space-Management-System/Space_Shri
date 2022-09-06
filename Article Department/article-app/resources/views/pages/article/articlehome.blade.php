58c7d8dc8c56debccafd8e46a864ace02c4b44e9
Waruni98 <it20666538@my.sliit.lk>
2022-09-06 12:35:51 +0530

    complete insert article

--- Article Department/article-app/resources/views/pages/article/articlehome.blade.php
+++ Article Department/article-app/resources/views/pages/article/articlehome.blade.php
@@ -4,10 +4,17 @@
 <form >
   {{ csrf_field() }}
   <div class="container">
-      <div class="row">    
+      <div class="row"> 
       </div>
-      <section>
+      <section >
+         
           <div class="form-group">
+            <table>
+              <tr>
+                <center><h2 class="form-title" style="color: antiquewhite" ><strong>Add Article</strong></h2> </center>
+              </tr>
+            </table>
+            
               <div class="mb-5">
                   <label for="exampleFormControlInput1" class="form-label">Topic</label>
                   <input title="The topic of the article to be applied." type="text"
@@ -16,69 +23,58 @@
               </div>
               <div class="mb-5">
                   <label for="exampleFormControlInput1" class="form-label">Author</label>
-                  <select title="The name of author of the article to be applied."
-                      class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" name="author"
-                      required>
+                  <input title="The name of author of the article to be applied." type="text"
+                      class="form-control form-control-lg"  id="exampleFormControlInput1" name="author"
+                      placeholder="Enter Author's name..." required>
                   </select>
               </div>
               <div class="mb-5">
                   <label for="exampleFormControlInput1" class="form-label">Date</label>
-                  <input type="date"/>
                   <input title="The date of the article was written"
-                      class="form-control form-control-lg" id="exampleFormControlInput1" name="height"
+                      class="form-control form-control-lg" type="date" id="exampleFormControlInput1" name="height"
                       placeholder="xx/xx/xxxx" required>
               </div>
-              <div class="mb-5">
-                  <label for="exampleFormControlInput1" class="form-label">Country</label>
-                  <input title="The number of stages of the rocket must be entered."
-                      class="form-control form-control-lg" id="exampleFormControlInput1" name="stages"
-                      placeholder="Select the country.."   required>
-                      <select id="continent" name="continent">
-                        <option>select continent</option>
-                        <option value="AF">Africa</option>
-                        <option value="AN">Antarctica</option>
-                        <option value="AS">Asia</option>
-                        <option value="EU">Europe</option>
-                        <option value="OC">Australia</option>
-                        <option value="NA">North America</option>
-                        <option value="SA">South America</option>
-                    </select>
-                    
-              </div>
-              <div class="mb-5">
-                  <label for="exampleFormControlInput1" class="form-label">First Flight</label>
-                  <input title="The date of the first flight of the rocket should be entered."
-                      class="form-control form-control-lg" type="date" id="exampleFormControlInput1" name="date"
-                      required>
 
-              </div>
               <div class="mb-5">
-                  <label for="exampleFormControlInput1" class="form-label">Status</label>
-                  <select title="Select the status of the respective rocket from the drop-down list."
-                      class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" name="status"
-                      required>
-                      <option selected>Open this status select menu</option>
-                      <option value="Retired">Retired</option>
-                      <option value="Active">Active</option>
-                  </select>
+                <label for="exampleFormControlInput1" class="form-label">Country of Origin</label>
+                <select title="Select the country of manufacture of the respective rocket from the drop-down list."
+                    class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" name="country"
+                    required>
+                    <option selected>Open this country of origin select menu</option>
+                    <option value="United States">United States</option>
+                    <option value="France, ESA">France, ESA</option>
+                    <option value="Russia">Russia</option>
+                    <option value="China">China</option>
+                    <option value="India">India</option>
+                    <option value="Sri Lanka">Sri Lanka</option>
+                </select>
+            </div>
+
+              <div class="mb-5">
+                <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+                <input title="The description of the article to be applied." type="text"
+                      class="form-control form-control-lg"  id="exampleFormControlInput1" name="description"
+                      placeholder="Type Article description..." required>
               </div>
+
               <div class="mb-5">
                   <label for="formFileLg" class="form-label">Image</label>
                   <input title="Insert a picture of the rocket." class="form-control form-control-lg" type="file"
                       name="images" accept="image/jpg, image/jpeg, image/png" required>
               </div>
           </div>
+          <div class="col-md-12 text-center">
+            <div class="btn-group">
+                <p><input type="submit" title="Click this button to save the entered data." class="btn btn-warning btn-lg"
+                        value="ADD DETAILS">
+                   
+                </p>
+            </div>
+      
+        </div>
       </section>
   </div>
-  <div class="col-md-12 text-center">
-      <div class="btn-group">
-          <p><input type="submit" title="Click this button to save the entered data." class="btn btn-warning btn-lg"
-                  value="ADD DETAILS">
-             
-          </p>
-      </div>
-
-  </div>
+  
 </form>
 
 @endsection
@@ -89,12 +85,10 @@
   margin-top:0px;
 	font-family: sans-serif;	
 	background-image: url(Articleinsert1.png);
+  background-position: center;
   background-color: #333;
 	background-repeat: no-repeat;
-  background-size: 100%;
-	
-  
-  
+  background-size: cover
 }
 label {
             color: #fff;
@@ -113,13 +107,20 @@ label {
           margin-right:50px;
           padding:0;
         }
+      
+.form-title {
+  margin-top:0px;
+  text-align: center;
+  font-size: 30px;
+  font-color: rgb(19, 118, 105);
+}
         
-
+ 
 .container {
+  background-color:#1f0c0c;
 	width: 500px;
 	margin:0% 0% 3% 15%;
 	border-radius: 0px;
-	background-color: rgba(6, 30, 12, 0.1);
 	box-shadow: 0 0 17px rgb(120, 83, 200);
 }
 

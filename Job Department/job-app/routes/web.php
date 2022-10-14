9fbe2cea370513393a0dd747d17a1f6ad8033b19
dasunikawijesooriya <74725211+dasunikawijesooriya@users.noreply.github.com>
2022-10-14 16:18:33 +0530

    changeController

--- Job Department/job-app/routes/web.php
+++ Job Department/job-app/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\PagesController;
+use App\Http\Controllers\InterfaceController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\ApplicantController;
 use Illuminate\Support\Facades\Route;
@@ -17,43 +17,43 @@ use Illuminate\Support\Facades\Route;
 
 
 //Create_job
-Route::get('/jobinsert', [PagesController::class, 'indexjobinsert'])->name('jobinsert');
+Route::get('/jobinsert', [InterfaceController::class, 'indexjobinsert'])->name('jobinsert');
 //Save_job
 Route::post('/savejob', [JobController::class, 'storejob'])->name('jobStore');
 //RetView_Job
-Route::get('/job',[PagesController::class,'indexjobsview'])->name('jobview');
+Route::get('/job',[InterfaceController::class,'indexjobsview'])->name('jobview');
 
 //Job_deparment Home page
-Route::get('/',[PagesController::class,'homeview'])->name('home');
+Route::get('/',[InterfaceController::class,'homeview'])->name('home');
 
 //dashbord manager
-Route::get('/dashboard',[PagesController::class,'adminview'])->name('');
+Route::get('/dashboard',[InterfaceController::class,'adminview'])->name('');
 //Applicant List
-Route::get('/applicant',[PagesController::class,'applicantlist'])->name('applicantlist');;
+Route::get('/applicant',[InterfaceController::class,'applicantlist'])->name('applicantlist');;
 // Job apply
-Route::get('/apply',[PagesController::class,'jobApplyindex'])->name('Applicantinsert');
-Route::get('/jobreport', [PagesController::class, 'indexjobreport'])->name('jobreport');
+Route::get('/apply',[InterfaceController::class,'jobApplyindex'])->name('Applicantinsert');
+Route::get('/jobreport', [InterfaceController::class, 'indexjobreport'])->name('jobreport');
 Route::post('/saveApplicant', [ApplicantController::class, 'saveApplicant'])->name('saveApplicant');
 
 Route::delete('/applicant/{id}',[ApplicantController::class,'destroy']);
 Route::delete('/jobs/{id}',[JobController::class,'destroy']);
-//Route::get('/JobUpdate',[PagesController::class,'jobupdate']);
+//Route::get('/JobUpdate',[InterfaceController::class,'jobupdate']);
 
-Route::get('/view',[PagesController::class,'jobdetailsVeiw']);
-Route::get('/pdf',[PagesController::class,'pdfgenerate'])->name('pdf');
+Route::get('/view',[InterfaceController::class,'jobdetailsVeiw']);
+Route::get('/pdf',[InterfaceController::class,'pdfgenerate'])->name('pdf');
 
 //admin edit jobs
-Route::get('/JobUpdate/{id}', [PagesController::class, 'indexjobedit']);
+Route::get('/JobUpdate/{id}', [InterfaceController::class, 'indexjobedit']);
 //admin save-edit jobs
 Route::post('/JobUpdate/{id}', [JobController::class, 'storeedtjobs'])->name('storeedtjobs');
 
 
 //user read
-//Route::get('/jobhomeu',[PagesController::class,'indexjobhomeuser'])->name('jhomeu');
+//Route::get('/jobhomeu',[InterfaceController::class,'indexjobhomeuser'])->name('jhomeu');
 
 Route::get('/jobdetails/{id}', [JobController::class, 'jobdetails_function'])->name('jobdetails');
 
 //admin genarate report
-//Route::get('/jobreport', [PagesController::class, 'indexjobreport'])->name('jobreport');
+//Route::get('/jobreport', [InterfaceController::class, 'indexjobreport'])->name('jobreport');
 //pdf generate
-//Route::get('admin/pdf/jobreport', [PagesController::class, 'pdfgenerate'])->name('jobpdf');
+//Route::get('admin/pdf/jobreport', [InterfaceController::class, 'pdfgenerate'])->name('jobpdf');
